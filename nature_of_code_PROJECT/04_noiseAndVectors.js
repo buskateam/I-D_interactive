bdfb144b35bc8f7c4b111414206ab5a0c59fc40c
Buska Hub <34079080+buskateam@users.noreply.github.com>
2017-12-08 00:18:11 +1300

    8122017

--- nature_of_code_PROJECT/04_noiseAndVectors.js
+++ nature_of_code_PROJECT/04_noiseAndVectors.js
@@ -1,43 +1,40 @@
 
-var inc  = 0.01;
+var inc  = 0.1;
 var nq = 2;
 var scl = 20;
 var cols, rows;
+var fr;
 
 function setup(){
-createCanvas(500,500);
-//pixelDensity(1);
+createCanvas(300,300);
 cols = floor(width/scl);
 rows = floor(height/scl);
-
+fr = createP('');
 
  }
 
 function draw(){
-
+background(255);
 
   var yoff = 0;
 
-  //loadPixels();
+
   for(var y = 0; y <height; y++){
     var xoff = 0;
     for(var x = 0; x < width; x++){
       var index = (x+y*width)*4;
       var r = noise(xoff,yoff)*255;
-      /*pixels[index + 0 ] = 25;
-      pixels[index + 1 ] = r;
-      pixels[index + 2 ] = r;
-      pixels[index + 3 ] = 255;*/
-
+      var v = p5.Vector.fromAngle(random(TWO_PI));
       xoff +=inc;
-
+      stroke(0);
+      push();
+      translate(x*scl,y*scl);
+      rotate(v.heading());
+      line(0,0,scl,0);
+      pop();
     }
       yoff += inc;
 
-  }
-
-    //inc+=0.001;
-
-    noiseDetail(nq,inc);
-    //updatePixels();
+    }
+    fr.html(floor(frameRate()));
   }
